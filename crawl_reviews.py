4aed86cd1729694a41ff57119af458b9187cce49
Dong Zhou <Zhou.Dong@microsoft.com>
2021-01-13 11:36:37 +0800

    update on iclr decision

--- crawl_reviews.py
+++ crawl_reviews.py
@@ -12,23 +12,29 @@ driver = webdriver.Edge('msedgedriver.exe')
 df = pd.read_csv('paperlist.tsv', sep='\t', index_col=0)
 
 ratings = dict()
+decisions = dict()
 for paper_id, link in tqdm(list(df.link.items())):
-
     try:
         driver.get(link)
-
-        xpath = '//div[@id="note_children"]//span[@class="note_content_value"]'
+        xpath = '//div[@id="note_children"]//span[@class="note_content_value"]/..'
         cond = EC.presence_of_element_located((By.XPATH, xpath))
         WebDriverWait(driver, 60).until(cond)
 
         elems = driver.find_elements_by_xpath(xpath)
         assert len(elems), 'empty ratings'
-        elems = [x for x in elems if 'withdrawal' not in x.text]
-        ratings[paper_id] = pd.Series([int(x.text.split(':')[0]) for x in elems[::2]], dtype=int)
+        ratings[paper_id] = pd.Series([
+            int(x.text.split(': ')[1]) for x in elems if x.text.startswith('Rating:')
+        ], dtype=int)
+        decision = [x.text.split(': ')[1] for x in elems if x.text.startswith('Decision:')]
+        decisions[paper_id] = decision[0] if decision else 'Unknown'
     except KeyboardInterrupt:
         break
     except Exception as e:
         print(paper_id, e)
         ratings[paper_id] = pd.Series(dtype=int)
+        decisions[paper_id] = 'Unknown'
 
-pd.DataFrame(ratings).T.to_csv('ratings.tsv', sep='\t')
+df = pd.DataFrame(ratings).T
+df['decision'] = pd.Series(decisions)
+df.index.name = 'paper_id'
+df.to_csv('ratings.tsv', sep='\t')
